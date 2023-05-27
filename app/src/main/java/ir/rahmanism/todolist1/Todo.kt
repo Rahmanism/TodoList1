063ede14f435992f501f8ef4dcc89dc54329f270
Rahmanism <s.mostafa.rahmani@gmail.com>
2023-05-27 13:56:29 +0330

    Add TodoAdpater

--- app/src/main/java/ir/rahmanism/todolist1/Todo.kt
+++ app/src/main/java/ir/rahmanism/todolist1/Todo.kt
@@ -2,5 +2,5 @@ package ir.rahmanism.todolist1
 
 data class Todo(
     val title: String,
-    val isDone: Boolean = false
+    var isDone: Boolean = false
 )
--- app/src/main/java/ir/rahmanism/todolist1/TodoAdapter.kt
+++ app/src/main/java/ir/rahmanism/todolist1/TodoAdapter.kt
@@ -1,10 +1,11 @@
 package ir.rahmanism.todolist1
 
+import android.graphics.Paint.STRIKE_THRU_TEXT_FLAG
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import ir.rahmanism.todolist1.databinding.TodoItemBinding
-
 
 class TodoAdapter(
     private val todos: MutableList<Todo>
@@ -30,6 +31,18 @@ class TodoAdapter(
         )
     }
 
+    fun addTodo(todo: Todo) {
+        todos.add(todo)
+        notifyItemInserted(todos.size - 1)
+    }
+
+    fun deleteDoneTodos() {
+        todos.removeAll { todo ->
+            todo.isDone
+        }
+        notifyDataSetChanged()
+    }
+
     override fun getItemCount(): Int {
         return todos.size
     }
@@ -37,5 +50,20 @@ class TodoAdapter(
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
         val curTodo = todos[position]
         holder.bind(curTodo)
+        holder.itemBinding.apply {
+            toggleStrikeThrough(todoTitleTv, curTodo.isDone)
+            isDoneCb.setOnCheckedChangeListener { _, isChecked ->
+                toggleStrikeThrough(todoTitleTv, isChecked)
+                curTodo.isDone = !curTodo.isDone
+            }
+        }
+    }
+
+    private fun toggleStrikeThrough(todoTitleTv: TextView, isDone: Boolean) {
+        if (isDone) {
+            todoTitleTv.paintFlags = todoTitleTv.paintFlags or STRIKE_THRU_TEXT_FLAG
+        } else {
+            todoTitleTv.paintFlags = todoTitleTv.paintFlags and STRIKE_THRU_TEXT_FLAG.inv()
+        }
     }
 }
