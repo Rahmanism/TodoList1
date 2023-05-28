e35139c29cccae5caa8c51a3040473dc639c3624
Rahmanism <s.mostafa.rahmani@gmail.com>
2023-05-28 10:53:50 +0330

    Add view to main activity
    and add click event listeners
    Update readme

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 A simple Todo List app - for learning Android Development with Kotlin.  
 
+It doesn't save any data, so by closing the app data will be lost!  
+
 [Android](https://www.google.com/search?q=android) [Kotlin](https://www.google.com/search?q=kotlin)  
 
--- app/src/main/java/ir/rahmanism/todolist1/MainActivity.kt
+++ app/src/main/java/ir/rahmanism/todolist1/MainActivity.kt
@@ -2,10 +2,36 @@ package ir.rahmanism.todolist1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.recyclerview.widget.LinearLayoutManager
+import ir.rahmanism.todolist1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var todoAdapter: TodoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        var activityMain = ActivityMainBinding.inflate(LayoutInflater.from(this.applicationContext))
+        setContentView(activityMain.root)
+        todoAdapter = TodoAdapter(mutableListOf())
+
+        activityMain.apply {
+            todoItemsRv.adapter = todoAdapter
+            todoItemsRv.layoutManager = LinearLayoutManager(this@MainActivity)
+
+            addTodoBtn.setOnClickListener {
+                val todoTitle = todoTitleEt.text.toString()
+                if (todoTitle.isNotEmpty()) {
+                    val todo = Todo(todoTitle)
+                    todoAdapter.addTodo(todo)
+                    todoTitleEt.text.clear()
+                }
+            }
+
+            deleteDoneBtn.setOnClickListener {
+                todoAdapter.deleteDoneTodos()
+            }
+        }
     }
 }
